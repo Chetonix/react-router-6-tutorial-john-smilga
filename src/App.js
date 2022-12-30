6e770fdfc10309c35153d9b59d4fddf25cfab50b
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-12-30 15:02:45 +0530

    add all the code before products refactoring

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from "./pages/SingleProduct";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-
+import ProtectedRoute from "./pages/ProtectedRoute";
+import SharedProductLayout from "./pages/SharedProductLayout";
 function App() {
   const [user, setUser] = useState(null);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<SingleProduct />} />
-          <Route path="login" element={<Login setUser={setUser} />} />
-          <Route path="Dashboard" element={<Dashboard user={user} />} />
+
+          <Route path="products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
+
+          <Route path="login" element={<Login setUser={setUser}></Login>} />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
